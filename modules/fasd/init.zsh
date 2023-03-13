25ff25e1561d8a3b6c72485f04aac82fdac27d0e
Jinal Kothari <jinal@bitcraft.in>
2023-03-13 19:34:23 +0530

    undo changes to fasd

--- modules/fasd/init.zsh
+++ modules/fasd/init.zsh
@@ -18,8 +18,7 @@ fi
 #
 # Initialization
 #
-_FASD_FUZZY=3
-# clean cache by `rm ~/.cache/prezto/fasd-cache.zsh`
+
 cache_file="${XDG_CACHE_HOME:-$HOME/.cache}/prezto/fasd-cache.zsh"
 if [[ "${commands[fasd]}" -nt "$cache_file" \
       || "${ZDOTDIR:-$HOME}/.zpreztorc" -nt "$cache_file" \
@@ -29,7 +28,7 @@ if [[ "${commands[fasd]}" -nt "$cache_file" \
 
   # Set fasd completion init arguments, if applicable.
   if zstyle -t ':prezto:module:completion' loaded; then
-    init_args+=(zsh-ccomp zsh-ccomp-install zsh-wcomp zsh-wcomp-install posix-alias)
+    init_args+=(zsh-ccomp zsh-ccomp-install zsh-wcomp zsh-wcomp-install)
   fi
 
   mkdir -p "$cache_file:h"
@@ -41,19 +40,18 @@ source "$cache_file"
 
 unset cache_file init_args
 
-
-# function fasd_cd {
-#   local fasd_ret="$(fasd -d "$@")"
-#   if [[ -d "$fasd_ret" ]]; then
-#     cd "$fasd_ret"
-#   else
-#     print "$fasd_ret"
-#   fi
-# }
+function fasd_cd {
+  local fasd_ret="$(fasd -d "$@")"
+  if [[ -d "$fasd_ret" ]]; then
+    cd "$fasd_ret"
+  else
+    print "$fasd_ret"
+  fi
+}
 
 #
 # Aliases
 #
 
 # Changes the current working directory interactively.
-# alias j='fasd_cd -i'
+alias j='fasd_cd -i'
